92ec2e51099231a2c0fd5640e9b0e4e3d6aad3a7
L0RDRAN <diego.wawro@gmail.com>
2025-06-06 21:09:33 -0300

    Delete listas_archivos directory

--- listas_archivos/descargaArchivo.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def wget(url):
-    r = requests.get(url, allow_redirects=True)
-    with open(url[url.rfind('/') + 1::], 'wb') as f:
-        f.write(r.content)
-
-wget("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Curso_Introductorio_Datos/noticia.txt")
